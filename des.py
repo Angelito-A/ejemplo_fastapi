61ccc538cddb3a50dfef8aad9fc3be42665347ef
Angelito-A <angelalfonsodeavilahiguita@gmail.com>
2025-09-29 17:05:30 -0500

    Update des.py

--- des.py
+++ des.py
@@ -19,7 +19,7 @@ def get_db():
 
 @app.get("/")
 async def root():
-    return {"message": "Soy Hacker D:!"}
+    return {"explicate ANgel": "Soy Hacker D:!"}
 
 @app.get("/items")
 async def get_items():
@@ -44,4 +44,5 @@ if __name__ == "__main__":
     import uvicorn
     port = int(os.getenv("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
+
 
